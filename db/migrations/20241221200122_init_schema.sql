1708938ba1096cf0233df887c4309f1b8f8bbd9e
Siddharth Singh <siddharthsingh2014@gmail.com>
2024-12-22 05:24:21 +0530

    Added migration to initialize schema

---
+++ db/migrations/20241221200122_init_schema.sql
@@ -0,0 +1,70 @@
+-- +goose Up
+-- +goose StatementBegin
+----------
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- Create users table
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create products table
+CREATE TABLE products (
+    product_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create orders table
+CREATE TABLE orders (
+    order_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status VARCHAR(50) DEFAULT 'Pending',
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create order_items table
+CREATE TABLE order_items (
+    order_item_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    order_id UUID REFERENCES orders(order_id) ON DELETE CASCADE,
+    product_id UUID REFERENCES products(product_id) ON DELETE CASCADE,
+    quantity INT NOT NULL DEFAULT 1,
+    unit_price DECIMAL(10, 2) NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL
+);
+
+----------
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+----------
+
+-- Drop order_items table first (to avoid foreign key constraint errors)
+DROP TABLE IF EXISTS order_items;
+
+-- Drop orders table
+DROP TABLE IF EXISTS orders;
+
+-- Drop products table
+DROP TABLE IF EXISTS products;
+
+-- Drop users table
+DROP TABLE IF EXISTS users;
+
+----------
+-- +goose StatementEnd
